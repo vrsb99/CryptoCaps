f2f22b235b4bfcbb79ff681f6bcf8644e2d02f62
Vignesh <vigneshrsb99@gmail.com>
2022-07-11 00:36:44 +0800

    Created Layout
    Installed antd @ant-design/icons react-redux @reduxjs/toolkit axios chart.js html-react-parser millify moment react-chartjs-2

--- src/App.js
+++ src/App.js
@@ -1,8 +1,43 @@
-import React from 'react'
+import React from 'react';
+import { Route, Routes, Link } from 'react-router-dom';
+import { Layout, Typography, Space } from 'antd';
+
+import { NavBar, HomePage, Exchanges, Cryptocurrencies, CryptoDetails, News } from './components';
+import './App.css';
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className="app">
+      <div className="navbar">
+        <NavBar />
+      </div>
+      <div className="main">
+        <Layout>
+          <div className="routes">
+            <Routes>
+              <Route exact path="/" element={<HomePage />} />
+              <Route exact path="/exchanges" element={<Exchanges />} />
+              <Route exact path="/cryptocurrencies" element={<Cryptocurrencies />} />
+              <Route exact path="/crypto/:coinId" element={<CryptoDetails />} />
+              <Route exact path="/news" element={<News />} />
+            </Routes>
+          </div>
+        </Layout>
+
+      
+      <div className="footer" >
+        <Typography.Title level={5} style={{ color: 'white', textAlign: 'center' }}>
+          CryptoCap <br />
+          All rights reserved.
+        </Typography.Title>
+        <Space>
+          <Link to="/">Home</Link>
+          <Link to="/exchanges">Exchanges</Link>
+          <Link to="/news">News</Link>
+        </Space>
+      </div>
+      </div>
+    </div>
   )
 }
 
---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+export {default as NavBar} from './NavBar.jsx';	// import Navbar from './Navbar';
+export {default as Cryptocurrencies} from './Cryptocurrencies.jsx';	
+export {default as CryptoDetails} from './CryptoDetails.jsx';	
+export {default as Exchanges} from './Exchanges.jsx';	
+export {default as HomePage} from './HomePage.jsx';	
+export {default as News} from './News.jsx';	
