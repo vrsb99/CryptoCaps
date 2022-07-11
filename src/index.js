5f2ebd7e8a71782fd46b4fb7422bbc52eeda3db7
Vignesh <vigneshrsb99@gmail.com>
2022-07-11 11:07:44 +0800

    Coinranking API from RAPIDApi Implemented

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
 import App from './App';
 import 'antd/dist/antd.min.css';
+import store from './app/store.js'
 
 ReactDOM.render(
     <Router>
-        <App />
+        <Provider store={store}>
+            <App />
+        </Provider>
     </Router>
-, document.getElementById('root'));
+    , document.getElementById('root'));
---
+++ src/services/cryptoApi.js
@@ -0,0 +1,24 @@
+import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react'
+
+const cryptoApiHeaders = {
+		'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
+		'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com'
+};
+
+const baseUrl = 'https://coinranking1.p.rapidapi.com';
+
+const createRequest = (url) => ({url, headers: cryptoApiHeaders})
+
+export const cryptoApi = createApi({
+	reducerPath: 'cryptoApi',
+	baseQuery: fetchBaseQuery({baseUrl}),
+	endpoints: (builder) => ({
+		getCryptos: builder.query({
+			query: ()=> createRequest('/coins')
+		})
+	})
+});
+
+export const {
+	useGetCryptosQuery,
+} = cryptoApi;
