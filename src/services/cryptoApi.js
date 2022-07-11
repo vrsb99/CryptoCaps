d654bc25dafd185f43e13f661a8365e2b1de012e
Vignesh <vigneshrsb99@gmail.com>
2022-07-11 11:59:49 +0800

    Cryptocurrencies Page Completed

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -9,12 +9,13 @@ const baseUrl = 'https://coinranking1.p.rapidapi.com';
 
 const createRequest = (url) => ({url, headers: cryptoApiHeaders})
 
+
 export const cryptoApi = createApi({
 	reducerPath: 'cryptoApi',
 	baseQuery: fetchBaseQuery({baseUrl}),
 	endpoints: (builder) => ({
 		getCryptos: builder.query({
-			query: ()=> createRequest('/coins')
+			query: (count)=> createRequest(`/coins?limit=${count}`),
 		})
 	})
 });
